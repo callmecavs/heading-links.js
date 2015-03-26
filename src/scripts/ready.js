0d0b57429d9beeb409d7945626d1f1816aeb7870
Michael Cavalea <callmecavs@gmail.com>
2015-03-25 23:56:54 -0400

    add prototype - processes heading text, only kebab-case links

---
+++ src/scripts/ready.js
@@ -0,0 +1,45 @@
+'use strict';
+
+// CONSTRUCTOR
+
+function HeadingLinks( options ) {
+  // selector defaults
+  this._selector       = options.selector || 'h1, h2, h3';
+  this._headings       = document.querySelectorAll(this._selector);
+  this._headingsLength = this._headings.length;
+
+  // link defaults
+  this._case     = options.case || 'kebab-case';
+
+  // call to init
+  document.addEventListener('DOMContentLoaded', this.init(), false);
+}
+
+// METHODS
+
+HeadingLinks.prototype.init = function() {
+  // loop through headings
+  for(var index = 0; index < this._headingsLength; index++) {
+    // get node
+    var element = this._headings[index];
+
+    // get heading text
+    var elementText = element.textContent;
+
+    // convert to desired link case
+    switch(this._case) {
+      case 'kebab-case':
+        elementText = elementText.toLowerCase()                 // convert to lower case
+                                 .replace(/[^\w\s]/gi, '')      // remove special chars, but preserve spaces
+                                 .replace(/\s+/g, '-');         // replace spaces with dashes
+      break;
+    }
+
+    // add id attribute
+    this._headings[index].setAttribute('id', elementText);
+  }
+}
+
+HeadingLinks.prototype.destroy = function() {
+
+}
