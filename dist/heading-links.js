23315fca7c0bc2a56b8c2e508f1d973fec5fa085
Michael Cavalea <callmecavs@gmail.com>
2015-03-26 23:34:12 -0400

    add working mouse enter handler

--- dist/heading-links.js
+++ dist/heading-links.js
@@ -5,19 +5,43 @@
 function HeadingLinks( options ) {
   // defaults
   this._selector       = options.selector || 'h1, h2, h3';
-  this._hoverLinks     = options.hoverLinks || true;
+  this._hoverLinks     = options.hoverLinks || true;            // THIS IS BUGGED!
 
   // headings vars
   this._headings       = document.querySelectorAll(this._selector);
   this._headingsLength = this._headings.length;
 
   // hover handlers
-  this._hoverLinkMouseEnter = function() {
+  this._hoverLinkMouseEnter = function( event ) {
+    // get heading that was hovered on
+    var heading = event.target;
 
+    // save heading id and text
+    var headingID   = heading.id;
+    var headingText = heading.textContent;
+
+    // create link
+    var link = document.createElement('a');
+
+    // add link href attribute
+    var linkUrl = '//' + window.location.hostname + window.location.pathname + '#' + headingID;
+    link.setAttribute('href', linkUrl);
+
+    // set link text content
+    link.textContent = headingText;
+
+    // clear heading text content
+    heading.textContent = '';
+
+    // append link to heading
+    heading.appendChild(link);
   }
 
-  this._hoverLinkMouseLeave = function() {
+  this._hoverLinkMouseLeave = function( event ) {
+    // get heading that was hovered on
+    var heading = event.target;
 
+    // be careful about only removing the link with the id
   }
 
   // call to create
