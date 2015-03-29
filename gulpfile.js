17444a95d001b8ffa062d79039cff45b17e503a9
Michael Cavalea <callmecavs@gmail.com>
2015-03-29 14:42:55 -0400

    add minified css file, update gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,19 @@ var onError = function(error) {
   this.emit('end');
 };
 
+// minify css
+gulp.task('sass', function() {
+  return gulp.src('dist/heading-links.css')
+    .pipe(gulp.dest('dist'))
+    .pipe(plugins.rename(function(path) {
+      path.basename = "heading-links.min"
+    }))
+    .pipe(plugins.sass({ outputStyle: 'compressed' }))
+    .on('error', onError)
+    .pipe(gulp.dest('dist'))
+    .pipe(plugins.connect.reload());
+});
+
 // uglify script
 gulp.task('scripts', function() {
   return gulp.src('dist/heading-links.js')
@@ -32,8 +45,9 @@ gulp.task('server', function() {
 
 // watch js file
 gulp.task('watch', function() {
+  gulp.watch('dist/heading-links.css', ['sass']);
   gulp.watch('dist/heading-links.js', ['scripts']);
 });
 
 // default task
-gulp.task('default', ['server', 'scripts', 'watch']);
+gulp.task('default', ['server', 'sass', 'scripts', 'watch']);
